c35404849e037535445feb82594121f7da176f65
rasmus123d <59487370+RasmusKD@users.noreply.github.com>
2024-05-07 10:18:37 +0200

    Initial commit

---
+++ src/test/java/com/minkostplan/eksamensprojekt/EksamensprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minkostplan.eksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
