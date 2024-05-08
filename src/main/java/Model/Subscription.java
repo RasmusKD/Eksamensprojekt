057b60bbe6b714cf7079c617559369ddd1b5facb
Mathias <math649ivigtig@gmail.com>
2024-05-08 12:19:41 +0200

    Klasser

---
+++ src/main/java/Model/Subscription.java
@@ -0,0 +1,70 @@
+package Model;
+
+import java.util.Date;
+
+public class Subscription {
+    private int subscriptionId;
+    private int userId;
+    private Date startDate;
+    private Date endDate;
+    private String status;
+
+    public Subscription(int subscriptionId, int userId, Date startDate, Date endDate, String status) {
+        this.subscriptionId = subscriptionId;
+        this.userId = userId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    public int getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(int subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Subscription{" +
+                "subscriptionId=" + subscriptionId +
+                ", userId=" + userId +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
