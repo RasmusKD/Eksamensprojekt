142909102f5790e05818f27546979794af624c00
Mathias <math649ivigtig@gmail.com>
2024-05-08 12:19:41 +0200

    Klasser

---
+++ src/main/java/Model/Ingredients.java
@@ -0,0 +1,69 @@
+package Model;
+
+public class Ingredients {
+
+    private int ingredientId;
+    private String name;
+    private double fat;
+    private double proteins;
+    private double carbohydrates;
+
+    public Ingredients(String name, int ingredientId, double fat, double proteins, double carbohydrates) {
+        this.name = name;
+        this.ingredientId = ingredientId;
+        this.fat = fat;
+        this.proteins = proteins;
+        this.carbohydrates = carbohydrates;
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public double getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(double proteins) {
+        this.proteins = proteins;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredients{" +
+                "ingredientId=" + ingredientId +
+                ", name='" + name + '\'' +
+                ", fat=" + fat +
+                ", proteins=" + proteins +
+                ", carbohydrates=" + carbohydrates +
+                '}';
+    }
+}
---
+++ src/main/java/Model/Recipe.java
@@ -0,0 +1,93 @@
+package Model;
+
+public class Recipe {
+
+    private int recipeId;
+    private String title;
+    private String description;
+    private String ingredients;
+    private String method;
+    private String cookingTime;
+    private String imageUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(String cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "recipeId=" + recipeId +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", ingredients='" + ingredients + '\'' +
+                ", method='" + method + '\'' +
+                ", cookingTime='" + cookingTime + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+
+    public Recipe(int recipeId, String title, String description, String ingredients, String method, String cookingTime, String imageUrl) {
+        this.recipeId = recipeId;
+        this.title = title;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.method = method;
+        this.cookingTime = cookingTime;
+        this.imageUrl = imageUrl;
+
+
+    }
+}
