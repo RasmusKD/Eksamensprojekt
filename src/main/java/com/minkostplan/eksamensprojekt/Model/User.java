eb0aa9166a90e629507a0e3d2220f438b299260e
Lauge Maar <lam002@edu.zealand.dk>
2024-05-08 12:21:02 +0200

    Mapper

--- src/main/java/com/minkostplan/eksamensprojekt/Model/User.java
+++ src/main/java/com/minkostplan/eksamensprojekt/Model/User.java
@@ -1,4 +1,137 @@
 package com.minkostplan.eksamensprojekt.Model;
 
 public class User {
+
+    private int userid;
+    private int firstName;
+    private int lastName;
+    private String email;
+    private String password;
+    private int age;
+    private char gender;
+    private  double weight;
+    private double height;
+    private String activityLevel;
+    private boolean isEmployed;
+
+    public User(int userid, int firstName, int lastName, String email, int age, String password, char gender, double weight, double height, String activityLevel, boolean isEmployed) {
+        this.userid = userid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.password = password;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.activityLevel = activityLevel;
+        this.isEmployed = isEmployed;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(int firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(int lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setActivityLevel(String activityLevel) {
+        this.activityLevel = activityLevel;
+    }
+
+    public boolean isEmployed() {
+        return isEmployed;
+    }
+
+    public void setEmployed(boolean employed) {
+        isEmployed = employed;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid=" + userid +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", weight=" + weight +
+                ", height=" + height +
+                ", activityLevel='" + activityLevel + '\'' +
+                ", isEmployed=" + isEmployed +
+                '}';
+    }
+
+
 }
