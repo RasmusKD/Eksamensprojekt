87d4b9205bfde1aad3acd0cbcf940c65355ccaa0
Lauge Maar <lam002@edu.zealand.dk>
2024-05-09 14:38:44 +0200

    New method createUser

--- src/main/java/com/minkostplan/eksamensprojekt/EksamensprojektApplication.java
+++ src/main/java/com/minkostplan/eksamensprojekt/EksamensprojektApplication.java
@@ -1,13 +1,34 @@
 package com.minkostplan.eksamensprojekt;
 
+import com.minkostplan.eksamensprojekt.Model.User;
+import com.minkostplan.eksamensprojekt.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EksamensprojektApplication {
+
+	@Autowired
+	private UserService userService;  // Autowire UserService
 
 	public static void main(String[] args) {
 		SpringApplication.run(EksamensprojektApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo() {
+		return args -> {
+			// Opret bruger data
+			User newUser = new User(
+					1, "John", "Doe", "john.doe@example.com", "password123", 30, 'M', 70.0, 180.0, "Active", true, false
+			);
+
+			// Kalder createUser metode for at indsætte brugeren i databasen
+			userService.createUser(newUser);  // Use the autowired service instance
+			System.out.println("New user created successfully!");
+		};
+	}
 }
