fdab6ef24dca4c420efb7270a36e2ecb34a0347b
Mathias <math649ivigtig@gmail.com>
2024-05-16 20:17:06 +0200

    Harris Benedict
    Usecase html
    
    DashboardController
    
    UseCase

--- src/main/java/com/minkostplan/eksamensprojekt/Service/UseCase.java
+++ src/main/java/com/minkostplan/eksamensprojekt/Service/UseCase.java
@@ -5,9 +5,6 @@ import com.minkostplan.eksamensprojekt.Model.Recipe;
 import com.minkostplan.eksamensprojekt.Model.Subscription;
 import com.minkostplan.eksamensprojekt.Model.User;
 import com.minkostplan.eksamensprojekt.Repository.DBRepository;
-import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,6 +19,7 @@ public class UseCase {
 
     @Value("${stripe.cancel.url}")
     private String cancelUrl;
+
     private DBRepository dBRepository;
 
     @Autowired
@@ -40,13 +38,7 @@ public class UseCase {
     }
 
     public void updateUser(User user) {
-        if (currentUser != null) {
-            user.setUserId(currentUser.getUserId());
-            dBRepository.updateUser(user);
-            System.out.println("User information updated successfully.");
-        } else {
-            System.out.println("No user currently logged in. Unable to update user information.");
-        }
+        dBRepository.updateUser(user);
     }
 
     public User getUserById(int userId) {
@@ -87,7 +79,6 @@ public class UseCase {
         return currentUser;
     }
 
-
     public void createSubscription(Subscription subscription) {
         dBRepository.createSubscription(subscription);
     }
@@ -105,5 +96,54 @@ public class UseCase {
 
     public String getCancelUrl() {
         return cancelUrl;
+    }
+
+    public double calculateCalories(User user) {
+        double bmr;
+        if (user.getGender() == 'M') {
+            bmr = 88.362 + (13.397 * user.getWeight()) + (4.799 * user.getHeight()) - (5.677 * user.getAge());
+        } else {
+            bmr = 447.593 + (9.247 * user.getWeight()) + (3.098 * user.getHeight()) - (4.330 * user.getAge());
+        }
+
+        double activityMultiplier;
+        switch (user.getActivityLevel()) {
+            case 0:
+                activityMultiplier = 1.2;
+                break;
+            case 1:
+                activityMultiplier = 1.375;
+                break;
+            case 2:
+                activityMultiplier = 1.55;
+                break;
+            case 3:
+                activityMultiplier = 1.725;
+                break;
+            case 4:
+                activityMultiplier = 1.9;
+                break;
+            default:
+                activityMultiplier = 1.2; // Default to sedentary if activity level is unknown
+        }
+
+        double totalCalories = bmr * activityMultiplier;
+
+        switch (user.getGoal()) {
+            case 0: // Vægttab
+                totalCalories -= 500;
+                break;
+            case 1: // Vægtøgning
+                totalCalories += 500;
+                break;
+            case 2: // Muskelopbygning
+                totalCalories += 300; // Slight surplus for muscle gain
+                break;
+            case 3: // Vedligehold vægt
+                // No adjustment needed
+                break;
+        }
+
+        return totalCalories;
     }
 }
