b4610bde25ed236940eee886039f67719b6752d6
rasmus123d <59487370+RasmusKD@users.noreply.github.com>
2024-05-29 18:55:14 +0200

    javadoc Config
    Co-Authored-By: Lauge Bastian Maar <149599642+Laugard@users.noreply.github.com>
    Co-Authored-By: Math649i <146831734+Math649i@users.noreply.github.com>
    Co-Authored-By: jon376 <111493958+jon376@users.noreply.github.com>

--- src/main/java/com/minkostplan/eksamensprojekt/config/StripeConfig.java
+++ src/main/java/com/minkostplan/eksamensprojekt/config/StripeConfig.java
@@ -1,15 +1,22 @@
 package com.minkostplan.eksamensprojekt.config;
+
 import com.stripe.Stripe;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Konfigurationsklasse til Stripe API-integration.
+ */
 @Configuration
 public class StripeConfig {
 
     @Value("${stripe.api.key}")
     private String apiKey;
 
+    /**
+     * Initialiserer Stripe API-nøglen ved opstart.
+     */
     @PostConstruct
     public void init() {
         Stripe.apiKey = apiKey;
